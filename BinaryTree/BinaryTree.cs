fbe0403c058a008a7fa78750f03cbe97fcd30a58
unknown <jarydc333@gmail.com>
2019-03-20 09:22:38 -0500

    BinarySearchTree works to place all numbers in the appropriate place and allows the user to search for the item

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+
+        public class Node
+        {
+            public int Data;
+            public Node Left = null;
+            public Node Right = null;
+            public void DisplayNode()
+            {
+                Console.WriteLine(Data + " ");
+            }
+        }
+        public Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+            if(root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (data < current.Data)
+                    {
+                        current = current.Left;
+
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+                    }
+
+                    else
+                    {
+                        current = current.Right;
+                        if (current == null)
+                        {
+                            parent.Right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+            newNode.DisplayNode();
+        }
+
+        public void Search(int lookFor, Node root)
+        {
+            Node current = root;
+
+            while (true)
+            {
+                if (current == null)
+                {
+                    Console.WriteLine("The tree is currently empty");
+                    break;
+                }
+
+                else if (lookFor == root.Data)
+                {
+                    Console.WriteLine("Found: " + root.Data);
+                    break;
+                }
+
+                else if (lookFor < current.Data)
+                {
+                    current = current.Left;
+                    if (current == null)
+                    {
+                        Console.WriteLine("That number does not exist in the tree.");
+                        break;
+                    }
+                    else if (lookFor == current.Data)
+                    {
+                        Console.WriteLine("Found: " + current.Data);
+                        break;
+                    }
+                }
+                else
+                {
+                    current = current.Right;
+                    if (current == null)
+                    {
+                        Console.WriteLine("That number does not exist in the tree.");
+                        break;
+                    }
+                    else if (lookFor == current.Data)
+                    {
+                        Console.WriteLine("Found: " + current.Data);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree BT = new BinaryTree();
+
+            BT.Search(100,BT.root);
+            BT.Insert(50);
+            BT.Insert(37);
+            BT.Insert(76);
+            BT.Search(36,BT.root);
+            BT.Search(38, BT.root);
+            BT.Search(75, BT.root);
+            BT.Search(77, BT.root);
+            BT.Search(50, BT.root);
+            BT.Search(76, BT.root);
+            Console.ReadLine();
+        }
+    }
+}
